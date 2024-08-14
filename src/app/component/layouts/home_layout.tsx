7c68dcf6bcc6a2d32c1879caed133b0d3758fddb
myselfabhi <abhinavverma2024@gmail.com>
2024-08-15 03:16:19 +0530

    fix: vercel error navbar

--- src/app/component/layouts/home_layout.tsx
+++ src/app/component/layouts/home_layout.tsx
@@ -1,8 +1,10 @@
 "use client"
 
+import dynamic from "next/dynamic"
 import React, { useEffect } from "react"
-import Navbar from "../landing-page/navbar"
-import Footer from "../landing-page/footer"
+// Dynamically import Navbar and Footer components
+const Navbar = dynamic(() => import("../navbar"))
+const Footer = dynamic(() => import("../footer"))
 
 
 
